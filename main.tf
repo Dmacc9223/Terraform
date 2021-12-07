a99ac20b18747d51fa9e426bad9edb0e7a275a8f
Dmacc9223 <95461480+Dmacc9223@users.noreply.github.com>
2021-12-07 15:30:27 +0530

    comment added in a1

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
 }
-
+# this is the a1 branch
 resource "aws_instance" "example" {
   ami           = "ami-061ac2e015473fbe2"
   instance_type = "t2.micro"
